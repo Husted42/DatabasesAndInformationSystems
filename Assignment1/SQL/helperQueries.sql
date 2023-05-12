35b4bf49240cfb8fdd29eb2534e6798ea21b777e
Husted42 <115221407+Husted42@users.noreply.github.com>
2023-05-12 10:21:27 +0200

    Update helperQueries.sql

--- Assignment1/SQL/helperQueries.sql
+++ Assignment1/SQL/helperQueries.sql
@@ -2,9 +2,11 @@ SELECT * FROM Restaurants;
 SELECT * FROM Customer;
 SELECT * FROM Ratings2;
 SELECT * FROM Dishes;
+SELECT * FROM Cuisine;
 SELECT * FROM orders;
 SELECT * FROM Delivery;
 SELECT * FROM Couriers;
+SELECT * FROM ComposedOf;
 
 
 -- All dishes on Østerbro
