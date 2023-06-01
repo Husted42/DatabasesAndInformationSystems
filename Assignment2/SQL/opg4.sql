fa05f0598fda5ff6e879c0cdfae4572dad32a89a
AblaZe42 <118982447+AblaZe42@users.noreply.github.com>
2023-06-01 16:17:33 +0200

    ...

---
+++ Assignment2/SQL/opg4.sql
@@ -0,0 +1,15 @@
+SELECT * FROM laptop;
+SELECT * FROM pc;
+SELECT * FROM printer;
+SELECT * FROM product;
+
+-- a)
+INSERT INTO Product VALUES ('C', 1100, 'pc');
+INSERT INTO PC VALUES (1100, 3.2, 1024, 180, 2499);
+
+-- b)
+DELETE FROM PC WHERE hd < 100;
+
+-- c)
+
+
