9c035a6aa592c435fd41915ab32bee119376c0b3
Husted42 <115221407+Husted42@users.noreply.github.com>
2023-06-01 15:05:17 +0200

    ...

--- Assignment2/SQL/queries.sql
+++ Assignment2/SQL/queries.sql
@@ -9,10 +9,16 @@ SELECT pc.model, pc.speed AS GHz, pc.hd AS GB FROM pc
 WHERE pc.price < 1000; 
 
 -- b) Find the manufacturers of printers
-
+SELECT DISTINCT product.maker FROM product 
+WHERE product.model IN(
+	SELECT printer.model FROM printer
+);
 
 -- c) Find the manufacturer and speed of laptops with a hard disk of at least thirty gigabytes.
-
+SELECT laptop.model, product.maker, laptop.speed FROM laptop
+LEFT JOIN product
+ON laptop.model = product.model 
+WHERE hd > 30;
 
 -- d) Find the model number and price of all products (of any type) made by manufacturer B.
 SELECT product.model, laptop.price FROM product 
@@ -25,8 +31,16 @@ SELECT product.model, pc.price FROM product
 RIGHT JOIN pc ON pc.model = product.model WHERE maker = 'B';
 
 -- e) Find those manufacturers that sell Laptops, but not PCs 
+SELECT DISTINCT product.maker, product.type
+FROM product
+WHERE product.type = 'laptop' 
+AND product.maker NOT IN (SELECT maker FROM product WHERE type = 'pc');
 
 -- f) Find the laptops whose speed is slower than that of any PC 
+SELECT * FROM laptop
+WHERE laptop.speed < (
+	SELECT MIN(speed) FROM PC
+)
 
 -- g) Find the maker of the color printer with the lowest price
 SELECT product.maker FROM product WHERE product.model IN(
